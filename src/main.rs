0787111a98336eefa33528f490afde1c2f41cf24
Ibis Sunrosa <drsunrosa@gmail.com>
2022-08-24 19:31:53 -0500

    Persistent storage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use serde::{Serialize, Deserialize};
 
 #[derive(PartialEq)]
 enum GameState {
@@ -7,12 +8,18 @@ enum GameState {
     Won,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Stats {
+    guesses: i32
+}
+
 fn main() {
     let mut game_state = GameState::Title;
     let lower_number = 1;
     let higher_number = 100;
     let target_num = rand::thread_rng().gen_range(lower_number..=higher_number);
     let mut guesses = 0;
+    let mut stats: Stats = read_storage();
 
     println!("Number is between {}-{}.", lower_number, higher_number);
     game_state = GameState::Playing;
@@ -36,6 +43,7 @@ fn main() {
             }
         }
     }
+    stats.guesses += guesses;
 
     if game_state == GameState::Won {
         println!(
@@ -43,4 +51,17 @@ fn main() {
             guesses, target_num
         );
     }
+    println!("You've made {} guesses so far.", stats.guesses);
+
+    write_storage(&stats);
+}
+
+fn write_storage(stats: &Stats) {
+    let serialized = serde_json::to_string(stats).unwrap();
+    std::fs::write("stats.json", serialized).expect("Unable to write to file.");
+}
+
+fn read_storage() -> Stats {
+    let serialized = std::fs::read_to_string("stats.json").expect("Unable to read file.");
+    serde_json::from_str(&serialized).unwrap()
 }
